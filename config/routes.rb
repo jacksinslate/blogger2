c122e6ef7f5853755d055892d21682070f43cd94
Nicholas Anselmini <jacksin.slate@gmail.com>
2016-12-02 18:45:38 -0500

    Tagging feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ root to: 'articles#index'
   		resources :articles do
   resources :comments
 end
-
+ resources :tags
 	end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
